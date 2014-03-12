5c92042045bc2124d94aff53791c5d0273193b68
Ramon Gebben <ramon@ra-ge.net>
2014-03-12 16:55:13 -0400

    First init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RevealEditor::Application.config.secret_key_base = '3989ab8679a359e691cb380b7e031874424b44d425bc9b873c1c2055f994119804ad0aa4800ef5c00427bf720fe63b1bf89972c39019604265d04f98c7afdef1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RevealEditor::Application.config.session_store :cookie_store, key: '_reveal-editor_session'
